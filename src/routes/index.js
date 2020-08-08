8bfcdf1d543b4c5f62307caea0b3c48a9367df3d
HarpeBlue <hspe2507@gmail.com>
2020-08-08 11:33:27 -0500

    Se terminó el manejo de rutas.

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,8 @@ const router = async () => {
   header.innerHTML = await Header();
   let hash = getHash();
   let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  content.innerHTML = await render();
 }
 
 export default router;
